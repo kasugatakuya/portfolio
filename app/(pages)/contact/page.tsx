2f5d23d43a35c89d1aaa96b2f4b5d00a918869b6
春日きぬ代 <kinuyokasuga@shunjitsukinudais-MacBook-Air.local>
2024-09-28 20:43:22 +0900

    実装１

--- app/(pages)/contact/page.tsx
+++ app/(pages)/contact/page.tsx
@@ -5,6 +5,22 @@ export default function Contact() {
         <div className="text-5xl">CONTACT</div>
         <div className="mt-5 text-xl">〜Under Construction〜</div>
       </div>
+      {/* <div className="mx-5 text-center">
+        「励ましのお言葉」から「お仕事のご相談」まで、ポジティブかつ幅広い声をお待ちしております。
+      </div>
+      <div className=" mx-5 md:mx-20">
+        <div className=" mt-10">NAME(お名前)</div>
+        <input className=" h-[80px] w-full bg-gray-300" type="text" />
+        <div className=" mt-10">MAIL ADRESS(メールアドレス)</div>
+        <input className="h-[80px] w-full bg-gray-300" type="text" />
+        <div className=" mt-10">MESSAGE(メッセージ)</div>
+        <input className="h-[300px] w-full bg-gray-300" type="text" />
+        <div className="flex justify-center">
+          <button className="my-10 flex justify-center bg-amber-700 px-10 py-3 text-white">
+            送信する
+          </button>
+        </div>
+      </div> */}
     </main>
   )
 }
