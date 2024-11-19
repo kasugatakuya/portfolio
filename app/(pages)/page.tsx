0dc2dbe9d14fa56884d64d3d4f24ab26066acdcb
春日きぬ代 <kinuyokasuga@shunjitsukinudais-MacBook-Air.local>
2024-11-19 20:02:56 +0900

    地理タイピング追加

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+import Image from 'next/image'
 export default function Home() {
   const scrollTop = () => {
     window.scrollTo({
@@ -16,57 +17,75 @@ export default function Home() {
         </div>
       </div>
       <div className="text-3xl underline">制作物</div>
+      <div className="my-10 items-center justify-center border bg-green-500 p-10 md:flex">
+        <Image
+          src="/typing-game.png"
+          className="pb-5 md:mr-10"
+          width={400}
+          height={500}
+          alt="プログラミング道場"
+        />
+        <div className="md:w-[32rem]">
+          <div className="mb-3 border-b text-lg text-white">
+            地理タイピングゲーム
+          </div>
+          <div>タイピング練習を手軽にゲーム感覚で楽しめるサイト。</div>
+          <div>タイピングしながら,世界の国の国旗、世界遺産が学べる。</div>
+          <div> さらに、都道府県、県庁所在地、県花、県鳥、県木が学べる。 </div>
+          <div>Next.jsとTypeScriptで作成。</div>
+        </div>
+      </div>
       <div className=" my-10 items-center justify-center border bg-red-400 p-10 md:flex">
-        <img
+        <Image
           src="/blackjack.png"
           className="pb-5 md:mr-10"
           width={400}
           height={500}
           alt="BLACK JACK"
         />
-        <div>
+        <div className="md:w-[32rem]">
           <div className="mb-3 border-b text-lg text-white">BLACK JACK</div>
           <div>トランプのブラックジャクゲーム。</div>
           <div>JavaScriptで作成。</div>
         </div>
       </div>
       <div className=" my-10 items-center justify-center border bg-violet-500 p-10 md:flex">
-        <img
+        <Image
           src="/ec.png"
           className=" pb-5 md:mr-10"
           width={400}
           height={500}
           alt="PCサイト(デモ)"
         />
-        <div>
+        <div className="md:w-[32rem]">
           <div className="mb-3 border-b text-lg text-white">PCサイト(デモ)</div>
           <div>様々な服が買えるECサイトのデモ。</div>
           <div>React+Reduxで作成。</div>
         </div>
       </div>
       <div className=" my-10 items-center justify-center border bg-yellow-500 p-10 md:flex">
-        <img
+        <Image
           src="/stork.jpg"
           className="pb-5 md:mr-10"
           width={400}
           height={500}
           alt="STORK模写"
         />
-        <div>
+        <div className="md:w-[32rem]">
           <div className="mb-3 border-b text-lg text-white">STORK模写</div>
           <div>WordPressのテーマであるSTORKの模写。</div>
           <div>HTML・CSS・jQueryで作成。</div>
         </div>
       </div>
       <div className=" my-10 items-center justify-center border bg-blue-400 p-10 md:flex">
-        <img
+        <Image
           src="/webgame.png"
           className="pb-5 md:mr-10"
           width={400}
           height={500}
           alt="プログラミング道場"
         />
-        <div>
+        <div className="md:w-[32rem]">
           <div className="mb-3 border-b text-lg text-white">
             プログラミング道場
           </div>
@@ -75,6 +94,7 @@ export default function Home() {
           <div>Ruby on Rails・JavaScriptで作成。</div>
         </div>
       </div>
+
       <div className="mb-10">
         <a
           href="https://github.com/kasugatakuya"
@@ -83,6 +103,7 @@ export default function Home() {
           *実際に制作したアプリはこちら*
         </a>
       </div>
+
       <div
         className=" fixed bottom-10 right-10 size-8 bg-gray-400 pt-1 text-center "
         onClick={() => scrollTop()}
